72ccc9a13ed42dc5c0da1b8a46bc46e10d897b61
OBGCyber <yusufiste189@gmail.com>
2026-09-05 20:37:51 +0300

    Refactor Unchain-ETAP script for clarity and flow

--- Unchain-ETAP.sh
+++ Unchain-ETAP.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
+# Script etapadmin olarak çalışmıyorsa etapadmin'e geç
+if [ "$(whoami)" != "etapadmin" ]; then
+    SIFRE="etap+pardus!"
+    echo "$SIFRE" | su - etapadmin -c "bash '$0' --etapadmin"
+    exit $?
+fi
+
 echo "Unchain-ETAP programına hoşgeldiniz! Bu program ile E-kilit servisi durdurabilir ve şifresiz açabilirsiniz!"
 
 echo "----ÖNEMLİ BİLGİLENDİRME----"
 echo "Bu programın garantisi bulunmamaktadır. Cihazın bozulması taktirde bu programı çalıştıran öğrenci başına geleceklerden sorumludur. Lütfen buna göre çalıştırınız. Eğer çalıştırmakta emin değilseniz lütfen Ctrl ile C tuşlarına basarak durdurunuz veya terminali kapatınız. Sadece Pardus 23 için desteklidir"
 
-# --- Bekleme Süresi ---
+echo "----ETAPADMİN ŞİFRESİ----"
+SIFRE="etap+pardus!"
+
 echo "İşlem 3 saniye içinde başlayacaktır..."
 sleep 3
-# ----------------------
-
-echo "----ETAPADMİN ŞİFRESİ----"
-echo "Bu program servisi durdurması için şifre istiyecektir.Ancak program şifreyi kendisi girecektir.Ancak Çalışmazsa Varsayılan ETAP 23 şifresi şudur: etap+pardus!"
-
-#Bu komud sistemdeki e-kilit ve servislerini durdurur
-SIFRE="etap+pardus!"
 
 # E-kilit ve EbaQR servislerini durdurur
 echo "$SIFRE" | sudo -S systemctl stop eta-kilit.service ebaqr.service
@@ -25,4 +27,5 @@ echo "$SIFRE" | sudo -S systemctl stop mcnd.service etausblogin.service eta-righ
 echo "$SIFRE" | sudo -S systemctl disable mcnd.service etausblogin.service eta-right-click.service eta-shutdown.service
 
 echo "----İŞLEM TAMAMLANDI----"
-echo "E-Kilit Servisleri devre dışı bırakıldı. Bu tarz projeler için lütfen https://github.com/obgcyber hesabına destek verin!"
+echo "E-Kilit Servisleri devre dışı bırakıldı."
+echo "https://github.com/obgcyber hesabına destek verin!"
